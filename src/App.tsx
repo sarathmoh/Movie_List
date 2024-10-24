b8be1a830ae45f9b6893093674c8c9375772bd9f
sarath mohan <esarathmohan@gmail.com>
2024-10-24 17:21:55 +0530

    signup functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import AppRoutes from "./router";
+import './index.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <>
       <AppRoutes />
+      <ToastContainer/>
     </>
   );
 }
